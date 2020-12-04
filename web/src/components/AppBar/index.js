a30bcb5a34e2dbb611b1dbefa24490d4ba3bf0f3
LAZARO <lazarolimasilva2@gmail.com>
2020-12-04 18:40:53 -0300

    Alterado

--- web/src/components/AppBar/index.js
+++ web/src/components/AppBar/index.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import ReactDOM from "react-dom";
 import { Link } from 'react-router-dom'
 
 import './styles.css'
 
-function AppBar() {
+function AppBar() {  
   return (
     <div className="appbar-container">
 
       <div className="appbar-content">
         <h1>LOGO</h1>
-
+        <div className="appbar-search">
+            <input
+            type="text"
+            placeholder="O que você está procurando?"          
+            />    
+        </div>
         <div className="appbar-items">
-          <Link to="/"> Home </Link>
-          <Link to="/"> Home </Link>
           <Link to="/"> Home </Link>
           <Link to="login"> Login </Link>
         </div>
--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import {useHistory} from 'react-router-dom';
+import { Slide } from 'react-slideshow-image';
 
+import 'react-slideshow-image/dist/styles.css'
 import './styles.css';
-import AppBar from '../../components/AppBar';
+
+const slideImages = [
+  './images/slide_2.jpg',
+  'images/slide_3.jpg',
+  'images/slide_4.jpg'
+];
 
 function Home() {
     
@@ -12,13 +19,33 @@ function Home() {
       history.push('login')
       console.log('ola')
     }
-
     return (
       <div className="home-container">
-        <h1> Home </h1>
-        <button className='teste' onClick={()=> navigateToLogin()}> Navegar </button>
+        <div className="slide-container">
+        <Slide>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url("https://images.unsplash.com/photo-1594750018712-77643025beb6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9")`}}>
+              <span>Slide 1</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+              <span>Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+        </Slide>
+      </div>
+        {/* <h1> Home </h1>
+        <button className='teste' onClick={()=> navigateToLogin()}> Navegar </button> */}
       </div>
     );
   }
   
   export default Home;
+
+  // <div style={{'backgroundImage': `url("https://images.unsplash.com/photo-1594750018712-77643025beb6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9")`}}>
