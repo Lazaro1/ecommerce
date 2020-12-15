95ec8a6d0773d352cc70fe4b1bbb7772be022c38
Alvaro Ferreira <alvarojfjunior@gmail.com>
2020-12-15 21:51:17 -0200

    fix repository

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const routes = express.Router();
 
 const users = require('./controllers/Users')
+const create = require('./controllers/Users')
 
 routes.post('/users/login', users.login);
+routes.post('/users/create', users.createusers);
 
 module.exports = routes;
